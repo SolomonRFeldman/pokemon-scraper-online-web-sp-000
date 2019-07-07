82f834ca2fe8530d05dc3c79b4a6de9824fd7c10
Solomon Feldman <solomonrfeldman@gmail.com>
2019-07-07 16:07:50 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,7 +1,7 @@
 class Pokemon
   attr_accessor :name, :type, :db, :id
   
-  def initialize(name:, type:, hp: 60, db:, id: nil)
+  def initialize(name:, type:, db:, id: nil)
     @name = name
     @type = type
     @db = db
@@ -11,8 +11,8 @@ class Pokemon
   def self.save(name, type, db)
     # pokemon = Pokemon.new(name: name, type: type, db: db)
     sql = <<-SQL
-      INSERT INTO pokemon (name, type, hp)
-      VALUES (?, ?, ?)
+      INSERT INTO pokemon (name, type)
+      VALUES (?, ?)
     SQL
     db.execute(sql, name, type)
     # pokemon.id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
