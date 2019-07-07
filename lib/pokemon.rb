4c992f509bde36f9f97c64c5405e82c5caae0f33
Solomon Feldman <solomonrfeldman@gmail.com>
2019-07-07 15:45:13 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -12,7 +12,7 @@ class Pokemon
     pokemon = Pokemon.new(name: name, type: type, db: db)
     sql = <<-SQL
       INSERT INTO pokemon (name, type)
-      VALUES (?, ?, ?)
+      VALUES (?, ?)
     SQL
     db.execute(sql, name, type)
     pokemon.id = db.execute("SELECT last_insert_rowid FROM pokemon"[0][0])
